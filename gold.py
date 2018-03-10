dc810b3be43724631d13de64a08a2de094ff5ec6
rlyost <ry@ryost.us>
2018-03-09 22:49:43 -0800

    cleaning up HTML

--- gold.py
+++ gold.py
@@ -2,30 +2,44 @@ from flask import Flask, render_template, request, redirect, session
 import random
 
 app = Flask(__name__)
-app.secret_key = 'ThisIsSecret'
+app.secret_key = 'freelunch'
 
 @app.route('/')
 def home():
     if not session.get('your_gold'):
         session['your_gold'] = 0
+        session['activities'] = ""
+        session['temp'] = 0
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
 def check():
     if request.form['value'] == 'farm':
-        session['your_gold'] += random.randrange(10, 21)
+        session['temp'] = random.randrange(10, 21)
+        session['your_gold'] += session['temp']
+        session['activities'] += "Earned ",session['temp']," golds from the farm! ", time
     elif request.form['value'] == 'cave':
-        session['your_gold'] = random.randrange(5, 11)
+        session['temp'] = random.randrange(5, 11)
+        session['your_gold'] += session['temp']
+        session['activities'] += "Earned ",session['temp']," golds from the cave! ", time
     elif request.form['value'] == 'house':
-        session['your_gold'] = random.randrange(2, 6)
+        session['temp'] = random.randrange(2, 6)
+        session['your_gold'] += session['temp']
+        session['activities'] += "Earned ",session['temp']," golds from the house! ", time
     elif request.form['value'] == 'casino':
-        session['your_gold'] = random.randrange(0, 101)-50 
+        session['temp'] = random.randrange(0, 101)-50
+        session['your_gold'] += session['temp']
+        if session['temp'] > 0:
+            session['activities'] += "Earned ",session['temp']," golds from the house! ", time
+        else:
+            session['activities'] += "Entered a casino and lost",session['temp'],".....Ouch! ", time
     return redirect('/')
 
-# @app.route('/reset')
-# def reset():
-#     # reset the random number and redirect to the home page
-#     session['your_gold'] = 0
-#     return redirect('/')
+@app.route('/reset')
+def reset():
+    # reset the gold and activities redirect to the home page
+    session['your_gold'] = 0
+    session['activities'] = ""
+    return redirect('/')
 
 app.run(debug=True)
